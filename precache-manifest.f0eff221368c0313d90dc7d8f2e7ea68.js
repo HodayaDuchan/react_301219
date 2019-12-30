684124ece2f676285c65c6abadd790ad519caf25
Hodaya <hodaya.d98@gmail.com>
2019-12-30 21:23:03 +0200

    Updates

---
+++ precache-manifest.f0eff221368c0313d90dc7d8f2e7ea68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c3c4f3d8dcf29076c315c56a5ab085",
+    "url": "/uploudgithub/index.html"
+  },
+  {
+    "revision": "5e295d724f3d5c2b16fd",
+    "url": "/uploudgithub/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f8747d9904669fd7e3b",
+    "url": "/uploudgithub/static/js/2.aebde0df.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/uploudgithub/static/js/2.aebde0df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e295d724f3d5c2b16fd",
+    "url": "/uploudgithub/static/js/main.58f3c487.chunk.js"
+  },
+  {
+    "revision": "e8d512be2b7f314c304c",
+    "url": "/uploudgithub/static/js/runtime-main.0549e25d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uploudgithub/static/media/logo.5d5d9eef.svg"
+  }
+]);
